756dc96278966d3fb05e24a39dea7650d590ecff
Paulo Rendon Jr <60458159+Paulo-Rendon@users.noreply.github.com>
2022-06-28 21:14:19 -0700

    Add files via upload
    The basics of the website, after this I am comfortable making the site go live and work on visual updates later on

--- data.js
+++ data.js
@@ -2,29 +2,95 @@ var projects = [
         {
             "projectName": "Personal Website",
             "description": "I am creating a personal website, the one you are on currently",
+            "summary":"I am building this website to help further my understanding of web develeopment. This website will also function as my personal website where I will direct possible hires. It will second as my personal hub for all major projects I worked on either for school, or personal projects. I will also allow people on my site to download both my SSU transcript as well as my resume. I also want this site to show the current projects I am working on as motivation to constantly be working on at least 2 new projects at a time.",
             "languages": [
                 "HTML",
                 "CSS",
                 "Javascript"
             ],
-            "links": [
-                {"GitHub": "https://github.com/Paulo-Rendon/Personal_Site"},
-                {"Trello": "https://trello.com/b/gyOoexVY/personal-website"},
-                {"Figma": "https://www.figma.com/file/BVbkDqGunMEb2KpRO4PlTA/Untitled?node-id=0%3A1"},
-                {"Personal Site": "file:///C:/Users/Paulo/OneDrive/Desktop/Personal%20Projects/Website/index.html"}
-            ],
+            "links": {
+                "GitHub": "https://github.com/Paulo-Rendon/Personal_Site",
+                "Trello": "https://trello.com/b/gyOoexVY/personal-website",
+                "Figma": "https://www.figma.com/file/BVbkDqGunMEb2KpRO4PlTA/Untitled?node-id=0%3A1",
+                "Personal Site": "file:///C:/Users/Paulo/OneDrive/Desktop/Personal%20Projects/Website/index.html"
+    },
             "thumbnail": "images/personal_site_screenshot.png"
         },
         {
             "projectName": "Android Game",
             "description": "I am making a simple infinate runner game to better my understanding of Unity",
+            "summary":"This game is being built in Unity and serves as the first Unity game I am developing. This project is functioning as my introduction to Unity as well as model rigging with Blender. I am trying to keep the game simple by having it an infinite runner. I want to later implement pickups as well as new skins the user can equip. I eventually want to work on a more story driven game in Unity but felt that this infinite runner will function as a better interduction to the software.",
             "languages": [
                 "C#"
             ],
-            "links": [
-                {"GitHub": "https://github.com/Paulo-Rendon/InfiniteRunnerGame"},
-                {"Trello": "https://trello.com/b/jywDNbmJ/game-dev"}
+            "links": {
+                "GitHub": "https://github.com/Paulo-Rendon/InfiniteRunnerGame",
+                "Trello": "https://trello.com/b/jywDNbmJ/game-dev"
+    },
+            "thumbnail": "images/android_project.png"
+        },
+        {
+            "projectName": "Research on PDF payload injection",
+            "description": "This is a final project report for CS340. I made a tutorial like video on how to inject shell code into a PDF and show how it runs when opened on a Windows XP OS.",
+            "summary":"This project was a final for CS340. We were allowed to choose are own ideas for this research project and I picked a PDF payload injection because I found it very interesting how something like a PDF which is opened and downloaded by everyone online can be harmful to a computer. Of course the simple injection I did would never work against any modern PDF reader, it is alarming to think that such an attack was possible back when Windows XP was the best available OS.",
+            "languages": [
+                "N/A"
             ],
-            "thumbnail": "nothing yet"
+            "links": {
+                "YouTube": "https://www.youtube.com/watch?v=4DmPf02islI",
+                "Report": "https://docs.google.com/document/d/1ZQMe53vBAuP4dFT1m3yWOk1Op1IsTUOLextLhh6-LtY/edit?usp=sharing"
+                },
+            "thumbnail": "images/PDF_Injection.png"
+        },
+        {
+            "projectName": "Sudoku Checker",
+            "description": "This program takes in a TXT file that uses whitespace to format integers in a Sudoku solution. The program then checks for the valididity of the solution provided by either forking children or using threads depending on the flags set when compiled. This project is for CS450 and so the code is not public to avoid plagerism",
+            "summary":"This was a group project for CS450, and since this was not a project idea thought up by the group I will not have the code public anywhere to avoid plagerism. The program reads a TXT file that uses newline characters and whitespace to format integers in a Sudoku solution. The program checks the valididity of the provided solution by either forking children processes or using threads depending on the flag set during compiling. This project served as a more in depth use of parallel programming methods and understaing the benifits and extra care needed when parallel programming is involved",
+            "languages": [
+                "C"
+            ],
+            "links": {
+                },
+            "thumbnail": "images/sudoku_code.png"
+        },
+        {
+            "projectName": "Streamline",
+            "description": "A website created in collaberation with 3 other classmates for our senior capstone project. The website is meant to be used by children to find what streaming services have there favorite shows",
+            "summary":"This website was created in a group effort with 3 other classmates for our senior capstone project. Sadly the website is no longer public but the code is available on Github. This project was my introduction to HTML, CSS, and Javascript as well as how to use an API. The website was designed to be used by young children so the color pallet is very colorful and attention grabing. It is a site meant to find a child friendly show/movie and show what streaming service offers said show/movie as well as the price.",
+            "languages": [
+                "HTML",
+                "Javascript",
+                "CSS"
+            ],
+            "links": {
+                "GitHub": "https://github.com/arniehayes/CS470",
+                "Trello": "https://trello.com/b/mfqHNt2i/project-streamline"
+                },
+            "thumbnail": "images/streamline.png"
+        },
+        {
+            "projectName": "SimWorld",
+            "description": "A video game that three other classmates and I worked on for CS370. This course was right when COVID started so half of this project was done completely online using Zoom to communicate with my partners.",
+            "summary": "This video game was a group project with 3 other classmates for our CS370 final. This project functions as my introduction to Unity, as well as agile programming and the use of Kanban Boards to keep everything organised. This project is also the first project I worked on in a romote setting with COVID moving the course to remote halfway through the semester. Since this project was a group idea the code is public on the Github link provided bellow.",
+            "languages": [
+                "C#"
+            ],
+            "links": {
+                "GitHub": "https://github.com/Squirtle-Squad-CS370/370World",
+                "Kanban Board": "https://docs.google.com/spreadsheets/d/1ImLBI8bQsUMFn7IJfHlj6Gxr2xxHwMh9dUvu5A68g_s/edit?usp=sharing"
+    },
+            "thumbnail": "images/Simworld.png"
+        },
+        {
+            "projectName": "Othello Game",
+            "description": "A video game that I designed for CS215. The game framework was provided, my job was to make it unique by adding designs and music as well as writing the AI that will play against the user. This project was the beginning of our use of Visual Studios. I kept the code private to avoid any plagerism since it was a course final project",
+            "summary":"This video game was designed for CS215. The game framework was provided, but any personal design choices like the background music and color sheme were my additions. I was also in charge of implementing an AI to play the player in the game of Othello. This functioned as my introduction to AI programming and I chose to go for a decision tree approach by calculating a value for every possible move the AI can make, with the value being based on how many of the players pieces the AI can convert as well as how open the move leaves my AI to future attacks by the user. After this is picks the move with the highest value and acts accordingly.",
+            "languages": [
+                "C++"
+            ],
+            "links": {
+                "GitHub": "https://github.com/Paulo-Rendon/CS215_Game"
+            },
+            "thumbnail": "images/othello_game.png"
         }
     ];
--- home.js
+++ home.js
@@ -1,3 +1,6 @@
+var projIndex = 2;
+const queryString = window.location.search.substr(1);
+
 function weWillGo(section){
     var sec = document.getElementById(section);
     var offset = window.innerHeight * .1;
@@ -5,6 +8,16 @@ function weWillGo(section){
     window.scrollTo({top: y, behavior: 'smooth'});
 }
 
+function download(path, name){
+    console.log("running download");
+    const anchor = document.createElement('a');
+    anchor.href = path;
+    anchor.download = name;
+    anchor.target = "_blank";
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
+}
 
 async function printData(){
     document.getElementById("proj1_title").innerHTML = projects[0].projectName;
@@ -25,5 +38,69 @@ async function printData(){
 
 /*After Page Loaded*/
 document.addEventListener('DOMContentLoaded', function() {
-   printData();
+    printData();
+    buildProjects();
+    if(queryString){
+        console.log(queryString);
+        weWillGo(queryString);
+    }
+    else
+        console.log("No Scroll");
 }, false);
+
+function buildProjects(){
+    for(var tmp = 0; tmp < 4 && projIndex + tmp < projects.length; tmp++){
+            document.getElementById("prev_projects").innerHTML += `
+            <div class=\"prev_proj_container\" onClick = \"openProject(${projIndex + tmp})\">
+                <p class=\"proj_title\">${projects[projIndex + tmp].projectName}</p>
+                <p class=\"proj_desc\">${projects[projIndex + tmp].description}</p>
+            </div>`;
+    }
+    
+    if(projIndex > 2)
+        document.getElementById("left_arrow").style = "color: rgba(255, 255, 255, 1)";
+    else
+        document.getElementById("left_arrow").style = "color: rgba(255, 255, 255, 0)";
+    
+    if(projIndex + 4 < projects.length)
+        document.getElementById("right_arrow").style = "color: rgba(255, 255, 255, 1)";
+    else
+        document.getElementById("right_arrow").style = "color: rgba(255, 255, 255, 0)";
+}
+
+function openProject(index){
+    indexOfOpenedProject = index;
+    window.location.href = 'proj.html?projIndex=' + index;
+}
+
+/*
+<div class="prev_proj_container">
+                    <p class="proj_title">Test Title</p>
+                    <p class="proj_desc">Test Description</p>
+                </div>
+*/
+
+function shiftProj(direction){
+    if(direction == "right"){
+        if(projIndex + 4 > projects.length)
+            console.log("Cannot shift left");
+        else{
+            projIndex += 4;
+
+        }
+    }
+    else if(direction == "left"){
+        if(projIndex - 4 < 2)
+            console.log("Cannot shift right");
+        else{
+            projIndex -= 4;
+
+        }
+    }
+    else{
+        console.log("not a valid argument");
+        return;
+    }
+    document.getElementById("prev_projects").innerHTML = "";
+    buildProjects(); 
+}
